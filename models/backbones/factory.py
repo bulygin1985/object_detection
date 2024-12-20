57b35b380dabe7343ba5fcfd7e3b6d7cc9abf8a9
k.hryshko <k.hryshko@steel.mil.ua>
2024-12-21 00:07:24 +0200

    more information in factory error

--- models/backbones/factory.py
+++ models/backbones/factory.py
@@ -4,4 +4,4 @@ from .centernet_backbone import Backbone
 def create_bakbone(backbonename: str, alpha: float):
     if not backbonename or backbonename == "default":
         return Backbone(alpha)
-    raise ValueError(f"Backbone {backbonename} is not supported yet.")
+    raise ValueError(f"Backbone '{backbonename}' is not supported yet.")
