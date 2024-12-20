d6a8be1c9e99679f5179fb963a7479668af81eac
k.hryshko <k.hryshko@steel.mil.ua>
2024-12-21 00:14:27 +0200

    cleaner abstract backbone

--- models/backbones/abstract_backbone.py
+++ models/backbones/abstract_backbone.py
@@ -1,11 +1,11 @@
-import abc
+from abc import ABC, abstractmethod
 
 import torch.nn as nn
 
 
-class AbstractBackbone(nn.Module):
+class AbstractBackbone(nn.Module, ABC):
 
-    @abc.abstractmethod
+    @abstractmethod
     def forward(self, x):
         """run feature extraction on prepared image x.
         returns features at strides 2, 4, 8, 16, 32
