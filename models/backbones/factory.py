08b989a9546062926e63d49d41b5315f64c938f4
k.hryshko <k.hryshko@steel.mil.ua>
2024-12-27 12:59:50 +0200

    Add resnet backbone

--- models/backbones/factory.py
+++ models/backbones/factory.py
@@ -1,8 +1,12 @@
 from .centernet_backbone import Backbone
+from .resnet import create_resnet_backbone
 
 
 def create_bakbone(backbonename: str, alpha: float, weights: str = None):
     if not backbonename or backbonename == "default":
-        assert weights is None
+        assert not weights
         return Backbone(alpha)
+    if backbonename.startswith("resnet"):
+        assert alpha == 1.0, f"only alpha=1 is supported at the moment {backbonename}."
+        return create_resnet_backbone(backbonename, weights)
     raise ValueError(f"Backbone '{backbonename}' is not supported yet.")
---
+++ models/backbones/resnet.py
@@ -0,0 +1,34 @@
+import torchvision.models as models
+
+from .abstract_backbone import AbstractBackbone
+
+
+class ResnetBackbone(AbstractBackbone):
+    def __init__(self, model: models.ResNet):
+        super().__init__()        
+        self.model = model
+        self.filters = [64, 64, 128, 256, 512]
+
+    def forward(self, x):
+        # https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
+        m = self.model
+        x = m.conv1(x)  # conv stride 2, effective 2
+        x = m.bn1(x)
+        x = m.relu(x)
+        out_stride_2 = x
+        x = m.maxpool(x)  # stride 2, effective 4
+        x = m.layer1(x)  # stride 1, effective 4
+        out_stride_4 = x
+        x = m.layer2(x)  # stride 2, effective 8
+        out_stride_8 = x
+        x = m.layer3(x)  # stride 2, effective 16
+        out_stride_16 = x
+        x = m.layer4(x)  # stride 2, effective 32
+        out_stride_32 = x
+        return out_stride_2, out_stride_4, out_stride_8, out_stride_16, out_stride_32
+
+
+def create_resnet_backbone(name, weights=None):
+    assert name.startswith("resnet")
+    model = models.get_model(name, weights=weights)
+    return ResnetBackbone(model)
