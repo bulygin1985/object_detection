7f444859542bb831cec152f692c5c2e9619b335c
k.hryshko <k.hryshko@steel.mil.ua>
2024-12-27 15:26:17 +0200

    correct filters counting for resnets50 and above that used bottleneck

--- models/backbones/resnet.py
+++ models/backbones/resnet.py
@@ -3,11 +3,27 @@ import torchvision.models as models
 from .abstract_backbone import AbstractBackbone
 
 
+def layer_out_width(resnet_layer):
+    lastblock = resnet_layer[-1]
+    if isinstance(lastblock, models.resnet.BasicBlock):
+        width = lastblock.conv2.weight.shape[0]
+    elif isinstance(lastblock, models.resnet.Bottleneck):
+        width = lastblock.conv3.weight.shape[0]
+    if lastblock.downsample is not None:
+        print("downsample")
+        width = width // 2
+    return width
+
+
 class ResnetBackbone(AbstractBackbone):
     def __init__(self, model: models.ResNet):
         super().__init__()
         self.model = model
-        self.filters = [64, 64, 128, 256, 512]
+        self.filters = [64] + [
+            layer_out_width(layer)
+            for layer in [model.layer1, model.layer2, model.layer3, model.layer4]
+        ]
+        print(self.filters)
 
     def forward(self, x):
         # https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py
