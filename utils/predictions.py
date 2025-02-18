44d733a2418ef62c2b528dbb99e277b778bf3d5f
Kyrylo Gryshko <kyrylogryshko@gmail.com>
2025-02-18 09:44:09 +0200

    convert_pred2coco changes: improve speed when threshold is used, add progress logging in predictions stage (#56)

--- utils/predictions.py
+++ utils/predictions.py
@@ -1,8 +1,9 @@
 import torch
 import torchvision.transforms.v2 as transforms
+from tqdm import tqdm
 
 
-def get_predictions(device, model, dataset):
+def get_predictions(device, model, dataset, show_progress=False):
     """Get model predictions for the given dataset"""
     model.eval()
     transform = transforms.Compose(
@@ -13,6 +14,9 @@ def get_predictions(device, model, dataset):
     )
 
     predictions = []
+    if show_progress:
+        pbar = tqdm(total=len(dataset))
+
     for img, _ in dataset:
         # Apply transformations
         img = transform(img)
@@ -23,5 +27,7 @@ def get_predictions(device, model, dataset):
             pred = model(img)
 
         predictions.append(pred)
+        if show_progress:
+            pbar.update(1)
 
     return predictions
