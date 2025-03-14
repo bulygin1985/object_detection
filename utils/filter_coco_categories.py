552d0488768a072cef4b4e370bdf7757cdfe7dc9
kgryshko <kyrylogryshko@gmail.com>
2025-03-14 18:28:51 +0200

    filter categories as well, add docstrings

--- utils/filter_coco_categories.py
+++ utils/filter_coco_categories.py
@@ -2,19 +2,40 @@ import json
 import os
 import sys
 
+from typing import List, Dict
 
-def filter_coco_annotation_dict(data, categories):
-    annotations = data["annotations"]
-    new_annotations = [a.copy() for a in annotations if a["category_id"] in categories]
-    for a in new_annotations:
-        new_category_id = categories.index(a["category_id"]) + 1
-        a["category_id"] = new_category_id
+
+def filter_dict_array(data: List[Dict], key: str, categories: List[int]):
+    new_data = [d.copy() for d in data if d[key] in categories]
+    for d in new_data:
+        new_category_id = categories.index(d[key]) + 1
+        d[key] = new_category_id
+    return new_data
+
+
+def filter_coco_annotation_dict(data: Dict, categories: List[int]):
+    """Filter coco annotations data dict with specified categories
+    Args:
+        data (dict): dictionary of coco annotations
+        categories (list[int]): list of categories to keep
+    Returns:
+        dict with only only specified categories in annotations and categories.
+    """
     result = data.copy()
-    result["annotations"] = new_annotations
+    result["annotations"] = filter_dict_array(
+        data["annotations"], "category_id", categories
+    )
+    result["categories"] = filter_dict_array(data["categories"], "id", categories)
     return result
 
 
 def main():
+    """Filter coco annotations json with specified categories.
+    Filter one class examples:
+         python filter_coco_categies.py pascal_val2007.json 15
+    Filter 3 classes examples:
+         python filter_coco_categies.py pascal_val2007.json 10,12,15
+    """
     if len(sys.argv) != 3:
         print(
             "filter_coco_categies.\nUsage:\n\tpython filter_coco_categies.py <input_filename> <categories_list>"
